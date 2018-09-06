76fb46263a50af7e646eb856803a8c42c3d9576c
Bruno Rozendo <bruno@brunorozendo.com>
2018-09-06 18:06:33 -0300

    3.9. Desafio Validando a descrição
      - Adição de novos campos na tela de cervejas

--- src/main/java/com/brunorozendo/brewer/model/Cerveja.java
+++ src/main/java/com/brunorozendo/brewer/model/Cerveja.java
@@ -1,12 +1,19 @@
 package com.brunorozendo.brewer.model;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public class Cerveja {
 
   @NotBlank
   private String sku;
+  @NotBlank
+  @Size(max = 30)
   private String nome;
+  @NotBlank
+  @Size(max = 50)
+  private String descricao;
+
 
   public String getSku() {
     return sku;
@@ -22,5 +29,13 @@ public class Cerveja {
 
   public void setNome(String nome) {
     this.nome = nome;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
   }
 }
