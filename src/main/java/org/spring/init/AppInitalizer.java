044dfd49bbfb61a2f0356ecb4a663da99788d492
Bruno Rozendo <bruno@brunorozendo.com>
2018-09-06 18:14:19 -0300

    3.10. Resolvendo problemas de acentuação
      - Correção encode para maquinas windows

--- src/main/java/org/spring/init/AppInitalizer.java
+++ src/main/java/org/spring/init/AppInitalizer.java
@@ -1,6 +1,9 @@
 package org.spring.init;
 
+import javax.servlet.Filter;
+
 import org.spring.config.WebConfig;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppInitalizer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -18,5 +21,13 @@ public class AppInitalizer extends AbstractAnnotationConfigDispatcherServletInit
   @Override
   protected String[] getServletMappings() {
     return new String[] { "/" };
+  }
+
+  @Override
+  protected Filter[] getServletFilters() {
+    CharacterEncodingFilter filter = new CharacterEncodingFilter();
+    filter.setEncoding("UTF-8");
+    filter.setForceEncoding(true);
+    return new Filter[] { filter };
   }
 }
