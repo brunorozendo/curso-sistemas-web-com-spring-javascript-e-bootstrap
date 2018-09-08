39609c666057cc2bf3967162d2f9a23e89bd7229
Bruno Rozendo <bruno@brunorozendo.com>
2018-09-08 14:41:45 -0300

    5.3. Baixando e configurando o layout no projeto
      - Add dos css/javacripts

--- src/main/java/org/spring/config/WebConfig.java
+++ src/main/java/org/spring/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.spring5.ISpringTemplateEngine;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -58,6 +59,14 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
     return resolver;
 
   }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry
+        .addResourceHandler("/assets/**")
+        .addResourceLocations("/assets/");
+  }
+
 
 
 }
