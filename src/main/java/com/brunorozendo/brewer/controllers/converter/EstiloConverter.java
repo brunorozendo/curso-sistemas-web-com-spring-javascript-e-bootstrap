f9c495d355c879ca62db72e26b5bde1f58f2e81b
bruno rozendo <bruno@brunorozendo.com>
2018-11-14 01:13:46 -0200

    add more tests

--- src/main/java/com/brunorozendo/brewer/controllers/converter/EstiloConverter.java
+++ src/main/java/com/brunorozendo/brewer/controllers/converter/EstiloConverter.java
@@ -1,12 +1,19 @@
 package com.brunorozendo.brewer.controllers.converter;
 
 import com.brunorozendo.brewer.model.entity.Estilo;
+import java.util.function.Predicate;
 import org.springframework.core.convert.converter.Converter;
 
 public class EstiloConverter implements Converter<String, Estilo> {
   @Override
   public Estilo convert(String source) {
-    if (!source.isEmpty()) {
+    return getEstilo(
+        s -> !s.isEmpty(),
+        source);
+  }
+
+  public Estilo getEstilo(Predicate<String> valid, String source) {
+    if (valid.test(source)) {
       Estilo estilo = new Estilo();
       estilo.setId(Integer.valueOf(source));
       return estilo;
--- src/test/groovy/com/brunorozendo/brewer/controllers/converter/EstiloConverterTest.groovy
+++ src/test/groovy/com/brunorozendo/brewer/controllers/converter/EstiloConverterTest.groovy
@@ -2,18 +2,48 @@ package com.brunorozendo.brewer.controllers.converter
 
 import spock.lang.Specification
 
+import java.util.function.Predicate
+
 class EstiloConverterTest extends Specification {
 
-    def "Convert"() {
+    def "test convert usando pelo spring"() {
         given:
         def a = new EstiloConverter()
 
         when:
         def o = a.convert("10")
 
-
         then:
         o.id == 10
 
     }
+
+    def "test convert methodo interno false"() {
+        Predicate<String> validarValor = {
+            false
+        }
+        given:
+        def a = new EstiloConverter()
+
+        when:
+        def o = a.getEstilo(validarValor, null )
+
+        then:
+        o == null
+    }
+
+    def "test convert methodo interno valido"() {
+        Predicate<String> validarValor = {
+            true
+        }
+        given:
+        def a = new EstiloConverter()
+
+        when:
+        def o = a.getEstilo(validarValor, "10" )
+
+        then:
+        o.id == 10
+    }
+
 }
